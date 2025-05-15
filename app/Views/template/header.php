520d8ffdceda936122f3f4309e2d8c9859bac41b
jakenapay <jakemantesnapay@gmail.com>
2025-05-15 21:39:20 +0800

    Add initial template and thesis view files

---
+++ app/Views/template/header.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Thesis Repository</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">LPU Thesis Repository</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+         <li class="nav-item"><a class="nav-link" href="/thesis/list">Home</a></li>
+         <li class="nav-item"><a class="nav-link" href="/thesis/about">About</a></li>
+         <li class="nav-item"><a class="nav-link" href="/thesis/faq">FAQ</a></li>
+         <li class="nav-item"><a class="nav-link" href="/thesis/account">My Account</a></li>
+         <li class="nav-item"><a class="nav-link" href="/thesis/contact">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-4">
